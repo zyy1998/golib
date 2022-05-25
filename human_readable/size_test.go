c91930ed3bf92c3fc7ed195cd111880e0e35461e
zhaoyongyuan <1224500763@qq.com>
2022-05-26 04:42:24 +0800

    add testify dependency add human readable size

---
+++ human_readable/size_test.go
@@ -0,0 +1,23 @@
+// @Author: m-manu https://github.com/m-manu
+// @Origin: https://github.com/m-manu/go-find-duplicates/blob/main/bytesutil/human_readable_size_test.go
+package human_readable
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestFormats(t *testing.T) {
+	tests := map[int64][2]string{
+		-1:                        {"", ""},
+		0:                         {"0 B", "0 B"},
+		1_023:                     {"1023 B", "1.02 KB"},
+		2_140:                     {"2.09 KiB", "2.14 KB"},
+		2_828_382:                 {"2.70 MiB", "2.83 MB"},
+		2_341_234_123_412_341_234: {"2.03 EiB", "2.34 EB"},
+	}
+	for value, expectedValues := range tests {
+		assert.Equal(t, expectedValues[0], BinaryFormat(value))
+		assert.Equal(t, expectedValues[1], DecimalFormat(value))
+	}
+}
